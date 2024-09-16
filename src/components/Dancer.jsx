bf5e99b4f1145f1e6d64a7efc77b0b9139729aa4
Mungjin01 <beckaljm@naver.com>
2024-09-16 17:51:40 +0900

    feat: gsap으로 카메라 이동, timeline 조절

--- src/components/Dancer.jsx
+++ src/components/Dancer.jsx
@@ -5,8 +5,12 @@ import { useEffect, useRef } from "react";
 import { useRecoilValue } from "recoil";
 import { IsEnteredAtom } from "../stores";
 import { Loader } from "./Loader";
+import gsap from "gsap";
+import { useThree } from "@react-three/fiber";
 
+let timeline;
 export const Dancer = () => {
+  const three = useThree();
   const isEntered = useRecoilValue(IsEnteredAtom);
   const dancerRef = useRef(null);
   const { scene, animations } = useGLTF("models/dancer.glb");
@@ -17,12 +21,37 @@ export const Dancer = () => {
   const scroll = useScroll();
   console.log(scroll);
 
-  useFrame(() => {});
+  useFrame(() => {
+    if (!isEntered) return;
+    timeline.seek(scroll.offset * timeline.duration());
+  });
 
   useEffect(() => {
     if (!isEntered) return;
     actions["wave"].play();
   }, [actions, isEntered]);
+
+  useEffect(() => {
+    if (!isEntered) return;
+    if (!dancerRef.current) return;
+    gsap.fromTo(
+      three.camera.position,
+      { x: -5, y: 5, z: 5 },
+      { x: 0, y: 6, z: 12, duration: 2.5 }
+    );
+    gsap.fromTo(three.camera.rotation, { z: Math.PI }, { z: 0, duration: 2.5 });
+  }, [isEntered, three.camera.position, three.camera.rotation]);
+
+  useEffect(() => {
+    if (!isEntered) return;
+    if (!dancerRef.current) return;
+    timeline = gsap.timeline();
+    timeline.from(
+      dancerRef.current.rotation,
+      { y: -4 * Math.PI, duration: 4 },
+      0.5
+    );
+  }, [isEntered]);
 
   if (isEntered) {
     return (
